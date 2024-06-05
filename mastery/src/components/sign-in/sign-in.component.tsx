019caa10b583d983f6975d2003454a2d6d2bc0a1
Carlos Hernandez <carloshinzunza2@gmail.com>
2024-06-05 16:59:28 -0600

    implemented firebase auth, sign in component, firebase utils and libraries

--- mastery/src/components/sign-in/sign-in.component.tsx
+++ mastery/src/components/sign-in/sign-in.component.tsx
@@ -1,8 +1,22 @@
-const SignIn = ()=>{
-    return(
-        <>
-            <h1>Sign in page</h1>
-        </>
-    )
-}
-export default SignIn
+import React from "react";
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+
+import { createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    try {
+      const {user} = await signInWithGooglePopup();
+     const userDocRef = await createUserDocumentFromAuth(user)
+    } catch (error) {
+      console.error("Error signing in with Google", error);
+    }
+  };
+  return (
+    <>
+      <h1>Sign in page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google popup</button>
+    </>
+  );
+};
+export default SignIn;
